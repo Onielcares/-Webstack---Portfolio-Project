8e650375959511222564ddac0badb437c5426acb
surahj <odebodeopeyemi@gmail.com>
2023-03-11 17:29:54 +0100

    Setup mongodb connection to mongodbAtlas

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,4 @@ app.get('/', (req, res) => {
   res.send('Welcome to easyfind app!');
 });
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+module.exports = app;
