c95a5780585f81b921611c63028a0e4fd4698a61
Grace <graceffiong@gmail.com>
2023-03-19 23:33:08 +0100

    make nav responsive

--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./index.scss";
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
-  )
-}
+    <div className="landingImg h-[3000px]">
+      <section className="">
+        <section className=" max-w-2xl flex flex-col gap-10 justify-center items-center text-center m-auto ">
+          <h2 className="font-semibold text-5xl md:text-8xl">Shop Easy</h2>
+          <p className="text-xl max-w-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </section>
 
-export default LandingPage
+        <section>
+          <h2>Featured Products</h2>
+          <p>“If you don’t try this app, you won’t become the superhero you were meant to be”</p>
+        </section>
+      </section>
+    </div>
+  );
+};
+
+export default LandingPage;
