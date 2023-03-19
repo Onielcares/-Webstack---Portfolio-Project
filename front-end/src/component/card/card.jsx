e742240c66a9477c417f6456de1810908ecc79ea
Grace <graceffiong@gmail.com>
2023-03-19 23:59:47 +0100

    card component

--- front-end/src/component/card/card.jsx
+++ front-end/src/component/card/card.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
   return (
-    <div>Card</div>
+    <div className='w-40'>
+      <div className='h-36 bg-black rounded-t-lg'></div>
+      <div className='flex flex-col gap-8 bg-white rounded-b-lg p-2'>
+        <p className='text-xl font-medium'>{props.product}</p>
+        <p className='text-sm'>Price: {props.price}</p>
+      </div>
+    </div>
   )
 }
--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -1,19 +1,46 @@
 import React from "react";
 import "./index.scss";
+import Card from "../component/card/card";
 
 const LandingPage = () => {
-  return (
-    <div className="landingImg h-[3000px]">
-      <section className="">
-        <section className=" max-w-2xl flex flex-col gap-10 justify-center items-center text-center m-auto ">
-          <h2 className="font-semibold text-5xl md:text-8xl">Shop Easy</h2>
-          <p className="text-xl max-w-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </section>
+  const data = [
+    {
+      product: 'Shoe',
+      price: '$120'
+    },
+    {
+      product: 'Clothings',
+      price: 'N300'
+    },
+    {
+      product: 'Automobile',
+      price: '$120'
+    },
+  ]
 
-        <section>
-          <h2>Featured Products</h2>
-          <p>“If you don’t try this app, you won’t become the superhero you were meant to be”</p>
-        </section>
+  return (
+    <div className="landingImg bg-beige">
+      <section className=" max-w-2xl flex flex-col gap-10 justify-center items-center text-center m-auto full_height ">
+        <h2 className="font-semibold text-5xl md:text-8xl">Shop Easy</h2>
+        <p className="text-xl max-w-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </section>
+
+      <section className="flex flex-col  gap-5">
+        <div className="text-center">
+          <h2 className="text-4xl font-medium mb-5">Featured Products</h2>
+          <p className="text-lg">
+            “If you don’t try this app, you won’t become the superhero you were
+            meant to be”
+          </p>
+        </div>
+        <div className="flex justify-center items-center gap-5">
+          {data.map((item, idx) => (
+            <Card key={idx} product={item.product} price={item.price} />
+          ))}
+        </div>
       </section>
     </div>
   );
