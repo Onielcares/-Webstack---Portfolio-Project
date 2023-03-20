edc7dabfd0a2490ca448ed6a9e3a9778d053f690
Peacebern <peace8282bern@gmail.com>
2023-03-20 23:32:05 +0000

    created category page

--- front-end/src/pages/Categories/Categories.jsx
+++ front-end/src/pages/Categories/Categories.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import "./categories.css"
 
 const Categories = () => {
   return (
-    <div>Categories</div>
+    <div className="category_container">
+      <div className="category_section1">
+
+      <div className="flex category_nav">
+        <h2 className="category_header">
+          Category
+        </h2>
+        <ul className="flex category_links">
+          <li><a href="" className="category_link">Lorem.</a></li>
+          <li><a href="" className="category_link">Lorem.</a></li>
+          <li><a href="" className="category_link">Lorem.</a></li>
+          <li><a href="" className="category_link">Lorem.</a></li>
+          <li><a href="" className="category_link">Lorem.</a></li>
+          <li><a href="" className="category_link">Lorem.</a></li>
+          <li><a href="" className="category_link">Lorem.</a></li>
+          <li><a href="" className="category_link">Lorem.</a></li>
+          <li><a href="" className="category_link">Lorem.</a></li>
+        </ul>
+      </div>
+      <form className="search_form">
+        <input type="text" name="search" id="search" className="search_input" placeholder="Search Category" />
+        <button className="search_btn"><i class="fa-solid fa-magnifying-glass"></i></button>
+      </form>
+      </div>
+
+    </div>
   )
 }
 
